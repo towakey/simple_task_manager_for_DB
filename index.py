1eaece44cabc733cc4e69e467d837e72cd3752a1
towakey <kyosgame@gmail.com>
2023-11-09 00:18:35 +0900

    taskファイルをフォルダに変更

--- index.py
+++ index.py
@@ -2,31 +2,32 @@
 
 import os
 
-task_folder_path = "./task"
+if __name__ == '__main__':
+    task_folder_path = "./task"
 
-files_file = [f for f in os.listdir(task_folder_path) if os.path.isfile(os.path.join(task_folder_path, f))]
+    files_file = [f for f in os.listdir(task_folder_path) if os.path.isdir(os.path.join(task_folder_path, f))]
 
-print("Content-Type: text/html\n")
-print("""
-<html>
-    <head>
-    </head>
-    <body>
-""")
-
-if len(files_file) > 0:
+    print("Content-Type: text/html\n")
     print("""
-<ul>
+    <html>
+        <head>
+        </head>
+        <body>
     """)
-    for file in files_file:
-        print("<li>"+file+"</li>")
-    print("""
-</ul>
-    """)
-else:
-    print("task not found")
 
-print("""
-    </body>
-</html>
-""")
+    if len(files_file) > 0:
+        print("""
+    <ul>
+        """)
+        for file in files_file:
+            print("<li>"+file+"</li>")
+        print("""
+    </ul>
+        """)
+    else:
+        print("task not found")
+
+    print("""
+        </body>
+    </html>
+    """)
