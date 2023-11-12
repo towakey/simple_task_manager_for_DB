f3e9698d5a8729efd1f85fc449993932d6538edd
towakey <kyosgame@gmail.com>
2023-11-13 00:40:18 +0900

    bootstrap5.3導入 iniファイルとコンテンツファイルから読み取るように変更

--- index.py
+++ index.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 import os
+import configparser
 
 if __name__ == '__main__':
     task_folder_path = "./task"
@@ -9,25 +10,41 @@ if __name__ == '__main__':
 
     print("Content-Type: text/html\n")
     print("""
-    <html>
-        <head>
-        </head>
-        <body>
+<html>
+    <head>
+        <link rel="stylesheet" href="./css/bootstrap.css">
+        <script src="./js/bootstrap.bundle.js"></script>
+    </head>
+    <body>
+        <div class="container">
     """)
 
     if len(files_file) > 0:
-        print("""
-    <ul>
-        """)
         for file in files_file:
-            print("<li>"+file+"</li>")
-        print("""
-    </ul>
-        """)
+            config = configparser.ConfigParser()
+            config.read('./task/'+file+'/config.ini')
+            content=""
+            f = open('./task/'+file+'/contents.txt', 'r', encoding='UTF-8')
+            print("""
+            <div class="card">
+                <div class="card-body">
+                    <h2 class="card-title">
+                        {file}
+                    </h2>
+                    <h5 class="card-subtitle">
+                        作成日:{create} 更新日:{update}
+                    </h5>
+                    <div class="card-text">
+                        {content}
+                    </div>
+                </div>
+            </div>
+            """.format(file=file,create=config['DATA']['CREATE_DATA'],update=config['DATA']['UPDATE_DATA'],content=f.read()))
     else:
         print("task not found")
 
     print("""
-        </body>
-    </html>
+        </div>
+    </body>
+</html>
     """)
